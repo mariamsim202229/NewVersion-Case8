b524c2ae4b8ce3ef227cbea075a7fad1cf56fec8
andsju <anders.sjunnesson@gmail.com>
2023-11-02 14:47:28 +0100

    CORS form request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import { TopNavigation } from "./components/TopNavigation";
+import { Login } from './pages/Login';
 
 function App() {
     return (
         <>
             <TopNavigation />
             <h1>Hello World</h1>
+            <Routes>
+                <Route path='/' element=''></Route>
+                <Route path='/login' element={<Login />}></Route>
+            </Routes>
         </>
     )
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+
+export function Login() {
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const endpoint = "http://localhost:4444/user/login";
+        const options = {
+            method: "POST",
+            mode: "cors",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name: name, password: password })
+        }
+        console.log("options", options);
+        asynqRequest(endpoint, options);
+    }
+
+    const asynqRequest = (endpoint, options) => {
+        fetch(endpoint, options)
+        .then(response => response.json())
+        .then((data) => {
+            console.log("data", data);
+        })
+    }
+
+    useEffect(() => {
+        console.log("name:", name, "password:", password);
+    }, [name, password]);
+
+    return (
+        <>
+            <h1>Login</h1>
+            <form action="" method="post" onSubmit={submitForm}>
+                <p>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" required value={name} onChange={(e) => setName(e.target.value)} />
+                </p>
+                <p>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </p>
+                <p>
+                    <input type="submit" value="Log in" />
+                </p>
+            </form>
+        </>
+    )
+}
