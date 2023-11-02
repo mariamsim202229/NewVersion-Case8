a0c47d24f5a35d71802f147e2a5d7e654a04b26a
andsju <anders.sjunnesson@gmail.com>
2023-11-02 13:41:21 +0100

    react reset, routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { TopNavigation } from "./components/TopNavigation";
+
+function App() {
+    return (
+        <>
+            <TopNavigation />
+            <h1>Hello World</h1>
+        </>
+    )
+}
+
+export default App;
---
+++ frontend/src/components/TopNavigation.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export function TopNavigation() {
+
+    return (
+        <nav>
+            <ul>
+                <li><Link to='/'>Start</Link></li>
+                <li><Link to='/login'>Login</Link></li>
+            </ul>
+        </nav>
+    )
+}
