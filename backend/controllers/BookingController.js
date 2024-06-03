4af4f2f2c5da4d8a70371a6e76fa254383e3fd72
mariamsim202229 <mariamsimonyan1234@gmail.com>
2024-06-03 12:00:25 +0200

    booking message update

--- backend/controllers/BookingController.js
+++ backend/controllers/BookingController.js
@@ -17,13 +17,26 @@ function handleCreateBooking(req, res) {
     }
 
     const newBooking = req.body;
-
     if (!newBooking.movieId || !newBooking.showId || !newBooking.seatNumber||!newBooking.username || !newBooking.email) {
         return res.status(501).send("Request did not succeed. Check your request body");
     }
-
     const createdBooking = bookings.createBooking(newBooking);
-    return res.send(createdBooking);
+
+    const message =  `Subject: Booking Created Successfully!
+
+    Dear ${createdBooking.username} Thank you for your booking!
+
+    Here are the booking details:
+
+    - Movie id: ${createdBooking.movieId}
+    - Show id: ${createdBooking.showId}
+    - Seatnumber: ${createdBooking.seatNumber}
+    - Email : ${createdBooking.email}
+
+    Regards,
+    [Cinema]
+`;
+    return res.send(message);
 }
 
 export { handleShowAllBookings, handleCreateBooking }
